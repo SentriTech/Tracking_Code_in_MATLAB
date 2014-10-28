407f2cee9a2d4374b14f2c5cb4b511ad7790dbae
wudan3551 <821158644@qq.com>
2014-10-28 20:31:30 +0800

    updatting
    updatting

--- Catch Time&Extract CSV file of each point/preProcess_v1_1.m
+++ Catch Time&Extract CSV file of each point/preProcess_v1_1.m
@@ -1,22 +1,29 @@
 clear;close;clc;
-%%%%%%=== following code are trying to extract rssi data when each point(totally 16) were walked through by a person  ===%%%%%%
-%%%%%% this will finaly generate pointNum csv file in the directory
-%step one:read obstacled time of each ponit
+%the following code are trying to extract each rssi data when
+% a people walk through each reference point(totally 16) from 
+% whole received dat！！！！data.csv .
+% this will finaly generate pointNum.csv file in this directory,
+%file number equal to reference point number.
+
+%Step one:Read time when people walked through each ponit
 catchTime = csvread('catchTime.csv');
-numNodes = 24;
-pointNum = 16;%human obstacle point number
+numNodes = 24;%CC2530 Node number
+pointNum = 16;%human obstacle point number(reference point number)
+%valid catchTime file??
 if(mod(length(catchTime),pointNum) == 2)
+%caculate time into 'second'
 circleNum = floor(length(catchTime)/pointNum);%number of circle people walk through 16 obstacle points
 obstacleTime = catchTime(:,2) * 3600 + catchTime(:,3) * 60 ...
 + catchTime(:,4) + catchTime(:,5)*10^(-3);
-%step two:get the time stamp of gathered data
+
+%Step two:Get the time stamp of gathered data！！！！data.csv
 fileName = 'data.csv';
 wholeData = csvread(fileName);
-%TimeStamps is the time when UART received data of node 1
+%TimeStamps is the time when PC received data of node 25(center node)
 TimeStamps = wholeData(wholeData(:,1)==1,2+numNodes) * 3600 + wholeData(wholeData(:,1)==1,2+numNodes+1) * 60 ...
 + wholeData(wholeData(:,1)==1,2+numNodes+2) + wholeData(wholeData(:,1)==1,2+numNodes+3)*10^(-3);
 
-%step three:generate csv file of each point
+%Step three:Generate csv file of each point
 for i = 1:pointNum
     dataTemp = [];
     for j = 0:(circleNum-1)
